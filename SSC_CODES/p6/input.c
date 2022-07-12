b39d49e3f9b14059d8fb88576fc9b3cd052271bc
ChinmaiJyothy <97036679+ChinmaiJyothy@users.noreply.github.com>
2022-07-12 21:31:16 +0530

    Add files via upload

---
+++ SSC_CODES/p6/input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,sum;
+
+// inputing two numbers
+printf("Enter the two numbers:");
+scanf("%d,%d",&n1,&n2);
+
+// calculating string
+sum=n1+n2;
+printf(sum=%d",sum);
+}
